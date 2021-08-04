ba8b496533702ba9a7357472f51ab646a9e63072
Bye <byemc@outlook.com>
2021-08-04 17:14:32 +0300

    Add files via upload

---
+++ amogus.py
@@ -0,0 +1,11 @@
+import random
+from random import randint
+
+random = randint(0, 100)
+
+for i in range(random):
+    print("Press Start as many times as this message appears to begin")
+
+for i in range(random):
+    input()
+
